c85f963d6f36c329d1ce10dfe736fd6aa45e8112
Edoardo T <endorama@users.noreply.github.com>
2020-04-20 17:34:01 +0200

    just remember that formula names are meaningful

--- Formula/gotunl.rb
+++ Formula/gotunl.rb
@@ -2,7 +2,7 @@ LATEST_RELEASE = "v1.2.1".freeze
 LATEST_VERSION = LATEST_RELEASE.gsub(/^v/, "").freeze
 LATEST_VERSION_SHA256 = "36f24d824b3440ae76a314bae95048e2c79e54912254da82a9f00c353dd4829d".freeze
 
-class GotunlVpn < Formula
+class Gotunl < Formula
   desc "Command line client for Pritunl"
   homepage "https://github.com/cghdev/gotunl"
   url "https://github.com/cghdev/gotunl/releases/download/#{LATEST_VERSION}/gotunl-linux-amd64.zip"
